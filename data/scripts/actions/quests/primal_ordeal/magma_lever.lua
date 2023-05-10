688e18ad7b0870aed3402b4fbbee1e6df5ebf3cb
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2023-05-10 00:52:27 +0000

    Dev: Magma Lever Temporary added

---
+++ data/scripts/actions/quests/primal_ordeal/magma_lever.lua
@@ -0,0 +1,84 @@
+-- Magma Bubble 
+
+local config = {
+	requiredLevel = 150,
+	daily = false,
+	roomCenterPosition = Position(33483, 31439, 15),
+	playerPositions = {
+		Position(33522, 31465, 15),
+		Position(33520, 31465, 15),
+        Position(33521, 31465, 15),        
+        Position(33523, 31465, 15),
+        Position(33524, 31465, 15)
+	},
+	teleportPosition = Position(33483, 31445, 15),
+	bossPosition = Position(33483, 31434, 15)
+}
+
+local leverboss = Action()
+
+function leverboss.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	if item.itemid == 1946 then		
+		-- Check if the player that pulled the lever is on the correct position
+		if player:getPosition() ~= config.playerPositions[1] then
+			player:sendTextMessage(MESSAGE_STATUS_SMALL, "You can\'t start the battle.")
+			return true
+		end
+		
+		local team, participant = {}
+
+		for i = 1, #config.playerPositions do
+			participant = Tile(config.playerPositions[i]):getTopCreature()
+			
+			-- Check there is a participant player
+			if participant and participant:isPlayer() then
+				-- Check participant level
+				if participant:getLevel() < config.requiredLevel then
+					player:sendTextMessage(MESSAGE_STATUS_SMALL,
+						"All the players need to be level ".. config.requiredLevel .." or higher.")
+					return true
+				end
+
+				-- Check participant boss timer
+				if config.daily and participant:getStorageValue(Storage.Boos.Plagueroot) > os.time() then
+					player:getPosition():sendMagicEffect(CONST_ME_POFF)
+					player:sendCancelMessage("Not all players are ready yet from last battle.")
+					return true
+				end
+
+				team[#team + 1] = participant
+			end
+		end
+
+		-- Check if a team currently inside the boss room
+		local specs, spec = Game.getSpectators(config.roomCenterPosition, false, false, 14, 14, 13, 13)
+		for i = 1, #specs do
+			spec = specs[i]
+			if spec:isPlayer() then
+				player:sendTextMessage(MESSAGE_STATUS_SMALL, "A team is already inside the boss room.")
+				return true
+			end
+
+			spec:remove()
+		end
+
+		-- Spawn boss
+		Game.createMonster("Brokul", config.bossPosition)		
+
+		-- Teleport team participants
+		for i = 1, #team do
+			team[i]:getPosition():sendMagicEffect(CONST_ME_POFF)
+			team[i]:teleportTo(config.teleportPosition)
+			-- Assign boss timer
+			team[i]:setStorageValue(Storage.Boos.Plagueroot, os.time() + 20*60*60) -- 20 hours
+		end
+		
+		config.teleportPosition:sendMagicEffect(CONST_ME_ENERGYAREA)
+	end
+
+	item:transform(item.itemid == 1946 and 1945 or 1946)
+	return true
+end
+
+leverboss:aid(34000)
+leverboss:register()
